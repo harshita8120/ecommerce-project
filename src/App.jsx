0b08b97f7e871236fdaab569a4929ced18e655f5
harshita8120 <harshita.ranjan25@gmail.com>
2026-03-19 22:45:23 +0530

    Upload Codebase

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { HomePage } from './pages/Home/HomePage';
+import { CheckoutPage } from './pages/Checkout/CheckoutPage';
+import { OrdersPage } from './pages/Orders/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+import './App.css'
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+  }
+
+  useEffect(() => {
+    loadCart();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+
+
+
+  )
+}
+
+export default App
